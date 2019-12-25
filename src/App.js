6f1a09c895dfd78ab69b191083be744710ec775e
Samuel Kraus <S_read@aol.com>
2019-12-24 22:17:07 -0600

    set up working skeleton for portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './Components/Header';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import Portfolio from './Components/Portfolio';
+import Resume from './Components/Resume';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing</h1>
     </div>
   );
 }
